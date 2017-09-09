0e06d1469bc2c42c3ec91ba8b7932fc89442773a
Sundeep7 <sundeep@wizni.com>
2017-09-09 21:34:40 +0530

    bus mapping demo

---
+++ data/loc.js
@@ -0,0 +1,62 @@
+﻿var stops = [
+          {
+              coords: { lat: 28.412839, lng: 77.041664 },
+              content: '<h4>Stop 1</h4>'
+          },
+          {
+              coords: { lat: 28.421507, lng: 77.039018 },
+              content: '<h4>Stop 2</h4>'
+          },
+          {
+              coords: { lat: 28.427017, lng: 77.037505 },
+              content: '<h4>Stop 3</h4>'
+          },
+          {
+              coords: { lat: 28.429536, lng: 77.040552 },
+              content: '<h4>Stop 4</h4>'
+          },
+          {
+              coords: { lat: 28.432287, lng: 77.046930 },
+              content: '<h4>Stop 5</h4>'
+          },
+          {
+              coords: { lat: 28.437011, lng: 77.045957 },
+              content: '<h4>Stop 6</h4>'
+          },
+          {
+              coords: { lat: 28.442948, lng: 77.038161 },
+              content: '<h4>Stop 7</h4>'
+          },
+          {
+              coords: { lat: 28.448240, lng: 77.038023 },
+              content: '<h4>Stop 8</h4>'
+          },
+          {
+              coords: { lat: 28.453622, lng: 77.043232 },
+              content: '<h4>Stop 9</h4>'
+          },
+          {
+              coords: { lat: 28.463078, lng: 77.052115 },
+              content: '<h4>Stop 10</h4>'
+          },
+          {
+              coords: { lat: 28.489606, lng: 77.080004 },
+              content: '<h4>Stop 11</h4>'
+          },
+          {
+              coords: { lat: 28.498286, lng: 77.088410 },
+              content: '<h4>Stop 12</h4>'
+          },
+          {
+              coords: { lat: 28.503162, lng: 77.088471 },
+              content: '<h4>Stop 13</h4>'
+          },
+          {
+              coords: { lat: 28.506467, lng: 77.083922 },
+              content: '<h4>Stop 14</h4>'
+          },
+          {
+              coords: { lat: 28.509734, lng: 77.079330 },
+              content: '<h4>Stop 15</h4>'
+          }
+        ];
---
+++ js/app.js
@@ -0,0 +1,124 @@
+﻿/*
+ Author: Sundeep
+ URL: 
+ */
+
+var map, marker;
+var startPos = [stops[0].coords.lat, stops[0].coords.lng];
+var speed = 50; // km/h
+
+var delay = 100;
+
+      
+function animateMarker(marker, km_h) {
+    var target = 0;
+    var km_h = km_h || 50;
+
+
+    function goToPoint() {
+        var lat = marker.position.lat();
+        var lng = marker.position.lng();
+        var step = (km_h * 1000 * delay) / 3600000; // in meters
+
+        var dest = new google.maps.LatLng(
+        stops[target].coords.lat, stops[target].coords.lng);
+
+        var distance =
+        google.maps.geometry.spherical.computeDistanceBetween(
+        dest, marker.position); // in meters
+
+        var numStep = distance / step;
+        var i = 0;
+        var deltaLat = (stops[target].coords.lat - lat) / numStep;
+        var deltaLng = (stops[target].coords.lng - lng) / numStep;
+
+        function moveMarker() {
+            lat += deltaLat;
+            lng += deltaLng;
+            i += step;
+
+            if (i < distance) {
+                marker.setPosition(new google.maps.LatLng(lat, lng));
+                setTimeout(moveMarker, delay);
+            }
+            else {
+                marker.setPosition(dest);
+                target++;
+                if (target == stops.length) { target = 0; }
+
+                setTimeout(goToPoint, delay);
+            }
+        }
+        moveMarker();
+    }
+            
+    goToPoint();
+            
+            
+}
+
+        
+
+function initialize() {
+           
+    var myOptions = {
+        zoom: 16,
+        center: new google.maps.LatLng(28.412839, 77.041664),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+
+    };
+    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+            
+
+            
+    marker = new google.maps.Marker({
+        position: new google.maps.LatLng(stops[0].coords.lat, stops[0].coords.lng),
+        map: map
+    });
+
+    // Loop through stops
+    for (var i = 0; i < stops.length; i++) {
+        // Add stop
+        addStop(stops[i]);
+    }
+
+    // Add Marker Function
+    function addStop(props) {
+        var stop = new google.maps.Marker({
+            position: props.coords,
+            map: map,
+            icon: './images/flag.png'
+                    
+                
+                    
+        });
+                
+                
+
+        // Check content
+        if (props.content) {
+            var infoWindow = new google.maps.InfoWindow({
+                content: props.content
+            });
+
+            stop.addListener('click', function () {
+                infoWindow.open(map, stop);
+            });
+        }
+    }
+            
+
+    google.maps.event.addListenerOnce(map, 'idle', function () {
+                
+        marker.setIcon('./images/bus.png');
+        animateMarker(marker, 400);//set speed
+        addStop({ coords: event });
+                
+                
+                
+    });
+}
+        
+
+initialize();
